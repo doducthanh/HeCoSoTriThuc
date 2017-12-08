f0e7a58d12f8e6e08f542a0efd5820a63e6fcc04
quangns <quangert1203@gmail.com>
2017-12-08 17:43:46 +0700

    Add: QueryFood

---
+++ TuVanMonAn/src/Model/QueryFood.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author quangns
+ */
+public class QueryFood {
+    
+    /**
+     * Tim kiem mon an theo ID
+     * @param ID ma cua mon an trong database
+     * @return Cac thuoc tinh cua mon an:
+     * tenmon, soluong, protein, tinh bot, lipit, nhom, diem, status
+     * @throws SQLException 
+     */
+    public static ArrayList<String> SearchFood(String ID) throws SQLException {
+        try(Connection conn = ConnectSQL.connectsql()) {
+            String query = "SELECT * FROM tbl_monan WHERE id = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, ID);
+            ResultSet rs;
+            ArrayList<String> food = new ArrayList<String>();
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String tenmon = rs.getString("tenmon");
+                String soluong = rs.getString("soluong");
+                String protein = rs.getString("protein");
+                String tinhbot = rs.getString("tinhbot");
+                String lipit = rs.getString("lipit");
+                String nhom = rs.getString("nhom");
+                String diem = rs.getString("diem");
+                String status = rs.getString("status");
+                food.add(ID);
+                food.add(tenmon);
+                food.add(soluong);
+                food.add(protein);
+                food.add(tinhbot);
+                food.add(lipit);
+                food.add(nhom);
+                food.add(diem);
+                food.add(status);
+            }
+            conn.close();
+            return food;
+        }
+        catch(Exception e) {
+            System.out.println("error: " + e);
+        }
+        return null;
+    } 
+    
+    
+    /**
+     * 
+     */
+    public static void InsertFood(String tenmon, String soluong, String protein,
+            String tinhbot, String lipit, String nhom) throws SQLException {
+        try(Connection conn = ConnectSQL.connectsql()) {
+            String query = "INSERT INTO tbl_monan(tenmon, soluong, protein, tinhbot, lipit, nhom)"
+                    + " VALUES(?,?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, tenmon);
+            ps.setString(2, soluong);
+            ps.setString(3, protein);
+            ps.setString(4, tinhbot);
+            ps.setString(5, lipit);
+            ps.setString(6, nhom);
+            ps.executeUpdate();
+            conn.close();
+        }
+        catch(Exception e) {
+            System.err.println("error: " + e);;
+        }
+    }
+    
+    public static void main(String[] args) throws SQLException {
+        ArrayList<String> food = SearchFood("1");
+        System.out.println(food.get(1));
+        InsertFood("che", "1 bat", "70", "90", "49", "1");
+    }
+}
